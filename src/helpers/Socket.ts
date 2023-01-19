0690a5bab62ad1e076b9b1d7b17bab416bb5ac62
LeandroTRibeiro <leandrothiago_ribeiro@hotmail.com>
2023-01-19 10:25:35 -0300

    height livechat att

--- src/helpers/Socket.ts
+++ src/helpers/Socket.ts
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
 
-export const socket = io('https://devchat.onrender.com');
-// export const socket = io('http://localhost:2000');
+// export const socket = io('https://devchat.onrender.com');
+export const socket = io('http://localhost:2000');
